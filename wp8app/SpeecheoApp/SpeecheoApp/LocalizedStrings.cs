30348cae7a0a3a95e9fc308bdec5cbfcaf13a246
Damien <damien.chaillou@gmail.com>
2013-03-30 17:57:54 +0100

    add windows phone 8

---
+++ wp8app/SpeecheoApp/SpeecheoApp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SpeecheoApp.Resources;
+
+namespace SpeecheoApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
