c907a395d5ed0885d9cbd7e1296795216234be67
Damien <damien.chaillou@gmail.com>
2013-04-13 00:32:30 +0200

    list, quit webservices

--- wp8app/SpeecheoApp/SpeecheoApp/Html/server.py
+++ wp8app/SpeecheoApp/SpeecheoApp/Html/server.py
@@ -1,18 +1,52 @@
 import tornado.ioloop
 import tornado.web
 import torndb
+import sys
 
 db = torndb.Connection("127.0.0.1", "speecheo",user="root",password="")
 
 class MapHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render("join.html")
+	def get(self):
+		#sys.exit()
+		self.render("join.html")
+		
+class WSUserQuitHandler(tornado.web.RequestHandler):
+	def get(self,confId,number):
+		query = "DELETE FROM `clientdata` WHERE `phone` = '"+number+"' AND `applicationId` = " + confId
+		try:
+			result = db.query(query)
+			self.write('{"result": "ok"}')
+		except :
+			self.write('{"result": "ko"}')
 
 class WSRegisterHandler(tornado.web.RequestHandler):
-    def get(self,confId,number,name):
-        result = db.query("INSERT INTO clientdata (applicationId,phone, pseudo) VALUES (" + str(confId) + ",'" + str(number) + "','" + str(name) + "');")
-		#self.write(number + "-"+name)
+	def get(self,confId,number,name):
+		self.write(confId + "-"+number + "-"+name)
+		try:
+			result = db.query("INSERT INTO clientdata (applicationId,phone, pseudo) VALUES (" + str(confId) + ",'" + str(number) + "','" + str(name) + "');")
+			self.write('{"result": "ok"}')
+		except :
+			self.write('{"result": "ko"}')
+			
+class WSListHandler(tornado.web.RequestHandler):
+	def get(self,confId):
+		try:
+			query = "SELECT pseudo,phone FROM clientdata WHERE applicationId=" + str(confId)
+			result = db.query(query)
+			json = "["
+			count = 1
+			for user in result:
+				if count < len(result):
+					json = json + '{"pseudo":"'+user["pseudo"] + '","phone":"'+user["phone"] + '"},'
+				else :
+					json = json + '{"pseudo":"'+user["pseudo"] + '","phone":"'+user["phone"] + '"}'
+				count = count + 1
+			json = json + "]"
+			self.write(json)
+		except :
+			self.write('{"result": "ko"}')
 
+		
 class WSDataHandler(tornado.web.RequestHandler):
     def get(self):
 		self.render("mapParis.json")
@@ -24,6 +58,8 @@ application = tornado.web.Application([
     (r"/", 		MapHandler),
     (r"/data", 	WSDataHandler),
     (r"/register/(.*?)/(.*?)/(.*?)", 	WSRegisterHandler),
+    (r"/quit/(.*?)/(.*?)", 	WSUserQuitHandler),
+    (r"/list/(.*?)", 	WSListHandler),
     (r"/img/(.*)", tornado.web.StaticFileHandler, {"path": "img/"})
 ])
 
